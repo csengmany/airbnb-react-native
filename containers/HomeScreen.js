54b107508cdf35c87bde6ea6a68bffaa41365c66
csengmany <sengmany.cathy@gmail.com>
2021-02-25 15:13:40 +0100

    rework homescreen

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -26,6 +26,22 @@ const height = Dimensions.get("window").height;
 export default function HomeScreen({ navigation }) {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const displayStars = (ratingValue) => {
+        const tab = [];
+        for (let i = 1; i <= 5; i++) {
+            if (ratingValue >= i) {
+                tab.push(
+                    <Entypo name="star" size={24} color={yellow} key={i} />
+                );
+            } else {
+                tab.push(
+                    <Entypo name="star" size={24} color={lightGrey} key={i} />
+                );
+            }
+        }
+
+        return tab;
+    };
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -50,160 +66,111 @@ export default function HomeScreen({ navigation }) {
             <ActivityIndicator size="large" color={red} />
         </View>
     ) : (
-        <View style={styles.container}>
-            <FlatList
-                data={data}
-                renderItem={({ item }) => {
-                    const yellowStars = new Array(item.ratingValue).fill(
-                        undefined
-                    );
-                    const greyStars = new Array(5 - item.ratingValue).fill(
-                        undefined
-                    );
-                    return (
-                        <TouchableOpacity
-                            style={styles.card}
-                            onPress={() =>
-                                navigation.navigate("Room", {
-                                    itemId: item._id,
-                                })
-                            }
+        <FlatList
+            style={{ backgroundColor: white }}
+            data={data}
+            keyExtractor={(item) => item._id}
+            renderItem={({ item }) => {
+                return (
+                    <TouchableOpacity
+                        style={styles.container}
+                        onPress={() =>
+                            navigation.navigate("Room", {
+                                itemId: item._id,
+                            })
+                        }
+                    >
+                        <ImageBackground
+                            style={styles.bgImage}
+                            source={{
+                                uri: item.photos[0].url,
+                            }}
                         >
-                            {/* <ImageBackground style={styles.bgImg} source={{ uri: "https://a2.muscache.com/im/pictures/a560cdc0-425d-4d7b-ab8a-f98481eeb23f.jpg" }} */}
-                            <ScrollView
-                                horizontal
-                                showsHorizontalScrollIndicator={false}
-                            >
-                                <View style={styles.imgCard}>
-                                    {item.photos.map((file, index) => {
-                                        return (
-                                            <Image
-                                                style={styles.img}
-                                                resizeMode="cover"
-                                                source={{ uri: file.url }}
-                                                key={file.picture_id}
-                                            />
-                                        );
-                                    })}
-                                </View>
-                                <Text style={styles.price}>{item.price} €</Text>
-                            </ScrollView>
+                            <View style={styles.price}>
+                                <Text style={styles.priceText}>
+                                    {item.price} €
+                                </Text>
+                            </View>
+                        </ImageBackground>
 
-                            <View style={styles.description}>
-                                <View style={styles.descriptionTxt}>
-                                    <Text
-                                        style={styles.title}
-                                        numberOfLines={1}
-                                    >
-                                        {item.title}
-                                    </Text>
-                                    <View>
-                                        <View style={styles.rating}>
-                                            {yellowStars.map((star, index) => {
-                                                return (
-                                                    <Entypo
-                                                        name="star"
-                                                        size={24}
-                                                        color={yellow}
-                                                        key={index}
-                                                    />
-                                                );
-                                            })}
-                                            {greyStars.map((star, index) => {
-                                                return (
-                                                    <Entypo
-                                                        name="star"
-                                                        size={24}
-                                                        color={lightGrey}
-                                                        key={index}
-                                                    />
-                                                );
-                                            })}
-                                            <Text style={styles.greyTxt}>
-                                                {item.reviews} reviews
-                                            </Text>
-                                        </View>
+                        <View style={styles.informations}>
+                            <View style={styles.informationsDetails}>
+                                <Text style={styles.title} numberOfLines={1}>
+                                    {item.title}
+                                </Text>
+                                <View>
+                                    <View style={styles.rating}>
+                                        {displayStars(item.ratingValue)}
+                                        <Text style={styles.greyTxt}>
+                                            {item.reviews} reviews
+                                        </Text>
                                     </View>
                                 </View>
-                                <Image
-                                    style={styles.owner}
-                                    resizeMode="cover"
-                                    source={{
-                                        uri: item.user.account.photo.url,
-                                    }}
-                                />
                             </View>
-                        </TouchableOpacity>
-                    );
-                }}
-                keyExtractor={(item) => item._id}
-            ></FlatList>
-        </View>
+                            <Image
+                                style={styles.profilePicture}
+                                source={{
+                                    uri: item.user.account.photo.url,
+                                }}
+                            />
+                        </View>
+                    </TouchableOpacity>
+                );
+            }}
+        ></FlatList>
+        // </View>
     );
 }
 const styles = StyleSheet.create({
     container: {
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    card: {
-        margin: 10,
-        width: width,
         borderBottomColor: lightGrey,
         borderBottomWidth: 1,
+        marginHorizontal: 10,
+        marginBottom: 20,
+        paddingBottom: 10,
+        marginTop: 10,
     },
-    imgCard: {
+    bgImage: {
         width: "100%",
-        height: 250,
-        flexDirection: "row",
-        position: "relative",
-    },
-    img: {
-        width: width - 20,
-        height: 220,
-        marginRight: 10,
+        height: 200,
+        justifyContent: "flex-end",
     },
     price: {
         backgroundColor: "black",
-        color: white,
-        position: "absolute",
-        bottom: 40,
-        padding: 10,
-        width: 70,
-        textAlign: "center",
-    },
-    description: {
-        width: width,
-        flexDirection: "row",
-        justifyContent: "space-around",
+        height: 40,
+        width: 85,
+        justifyContent: "center",
         alignItems: "center",
-        paddingRight: 10,
+        marginBottom: 20,
     },
-    descriptionTxt: {
-        width: "65%",
-        margin: 0,
+    priceText: {
+        color: white,
+        fontSize: 18,
     },
-    owner: {
+    profilePicture: {
         height: 80,
         width: 80,
-        borderRadius: 50,
-        margin: 10,
+        borderRadius: 40,
     },
-    title: {
-        fontSize: 18,
-        color: "black",
+    informations: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 10,
     },
-
+    informationsDetails: {
+        flex: 1,
+        justifyContent: "space-around",
+    },
     rating: {
         flexDirection: "row",
         alignItems: "center",
     },
+    title: {
+        fontSize: 18,
+    },
     greyTxt: {
         color: lightGrey,
-        fontSize: 14,
-    },
-    bgImg: {
-        width: "100%",
-        height: 200,
+        fontSize: 15,
+        marginLeft: 10,
     },
 });
--- containers/RoomScreen.js
+++ containers/RoomScreen.js
@@ -21,8 +21,28 @@ export default function RoomScreen({ route, navigation }) {
     // console.log(route.params.itemId);
     const [isLoading, setIsLoading] = useState(false);
     const [data, setData] = useState([]);
-    let yellowStars = [];
-    let greyStars = [];
+
+    const displayStars = (ratingValue) => {
+        const tab = [];
+        for (let i = 1; i <= 5; i++) {
+            if (ratingValue >= i) {
+                tab.push(
+                    <Entypo name="star" size={24} color={yellow} key={index} />
+                );
+            } else {
+                tab.push(
+                    <Entypo
+                        name="star"
+                        size={24}
+                        color={lightGrey}
+                        key={index}
+                    />
+                );
+            }
+        }
+
+        return tab;
+    };
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -32,8 +52,6 @@ export default function RoomScreen({ route, navigation }) {
                 );
                 console.log(response.data);
                 setData(response.data);
-                yellowStars = new Array(data.ratingValue).fill(undefined);
-                // greyStars = new Array(5 - data.ratingValue).fill(undefined);
                 setIsLoading(false);
             } catch (error) {
                 console.log(error.message);
@@ -49,69 +67,9 @@ export default function RoomScreen({ route, navigation }) {
             <ActivityIndicator size="large" color={red} />
         </View>
     ) : (
-        // <View style={styles.container}>
-        //     <Text>This is the RoomScreen component</Text>
-        //     <Text>{itemId}</Text>
-
-        //     <View style={styles.card}>
-        //         <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        //             <View style={styles.imgCard}>
-        //                 {data.photos.map((file, index) => {
-        //                     return (
-        //                         <Image
-        //                             style={styles.img}
-        //                             resizeMode="cover"
-        //                             source={{ uri: file.url }}
-        //                             key={file.picture_id}
-        //                         />
-        //                     );
-        //                 })}
-        //             </View>
-        //             <Text style={styles.price}>{data.price} €</Text>
-        //         </ScrollView>
-
-        //         <View style={styles.descriptionTxt}>
-        //             <Text style={styles.title} numberOfLines={1}>
-        //                 {data.title}
-        //             </Text>
-        //             <View>
-        //                 <View style={styles.rating}>
-        //                     {yellowStars.map((star, index) => {
-        //                         return (
-        //                             <Entypo
-        //                                 name="star"
-        //                                 size={24}
-        //                                 color={yellow}
-        //                                 key={index}
-        //                             />
-        //                         );
-        //                     })}
-        //                     {greyStars.map((star, index) => {
-        //                         return (
-        //                             <Entypo
-        //                                 name="star"
-        //                                 size={24}
-        //                                 color={lightGrey}
-        //                                 key={index}
-        //                             />
-        //                         );
-        //                     })}
-        //                     <Text style={styles.greyTxt}>
-        //                         {data.reviews} reviews
-        //                     </Text>
-        //                 </View>
-        //             </View>
-        //         </View>
-        //         <Image
-        //             style={styles.owner}
-        //             resizeMode="cover"
-        //             source={{
-        //                 uri: data.user.account.photo.url,
-        //             }}
-        //         />
-        //     </View>
-        // </View>
-        <View></View>
+        <View>
+            <Text>{itemId}</Text>
+        </View>
     );
 }
 
@@ -119,61 +77,5 @@ const styles = StyleSheet.create({
     container: {
         alignItems: "center",
         justifyContent: "center",
-    },
-    card: {
-        margin: 10,
-        width: width,
-        borderBottomColor: lightGrey,
-        borderBottomWidth: 1,
-    },
-    imgCard: {
-        width: "100%",
-        height: 250,
-        flexDirection: "row",
-        position: "relative",
-    },
-    img: {
-        width: width - 20,
-        height: 220,
-        marginRight: 10,
-    },
-    price: {
-        backgroundColor: "black",
-        color: white,
-        position: "absolute",
-        bottom: 40,
-        padding: 10,
-        width: 70,
-        textAlign: "center",
-    },
-    description: {
-        width: width,
-        flexDirection: "row",
-        justifyContent: "space-around",
-        alignItems: "center",
-        paddingRight: 10,
-    },
-    descriptionTxt: {
-        width: "65%",
-        margin: 0,
-    },
-    owner: {
-        height: 80,
-        width: 80,
-        borderRadius: 50,
-        margin: 10,
-    },
-    title: {
-        fontSize: 18,
-        color: "black",
-    },
-
-    rating: {
-        flexDirection: "row",
-        alignItems: "center",
-    },
-    greyTxt: {
-        color: lightGrey,
-        fontSize: 14,
     },
 });
